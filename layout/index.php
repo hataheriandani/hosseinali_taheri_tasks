26d7ab1562e8c1a31fc09d94bddbf3a288f5a491
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-01 10:01:50 +0200

    layout

---
+++ layout/index.php
@@ -0,0 +1,22 @@
+<?php include "header.php" ?>
+
+<h1> some content </h1>
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia 
+    obcaecati unde velit consequatur laboriosam sequi aperiam ratione,
+    perspiciatis et veritatis qui fugit vero? Praesentium, similique
+    quaerat deserunt debitis quos omnis! Lorem ipsum dolor sit, amet 
+    consectetur adipisicing elit.Nostrum rem optio voluptates aliquid
+    accusamus quo quis, eum fuga, aut error tempora magni consequuntur
+    nam magnam vitae culpa aliquam aspernatur minus?
+</p>
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia 
+    obcaecati unde velit consequatur laboriosam sequi aperiam ratione,
+    perspiciatis et veritatis qui fugit vero? Praesentium, similique
+    quaerat deserunt debitis quos omnis! Lorem ipsum dolor sit, amet 
+    consectetur adipisicing elit.Nostrum rem optio voluptates aliquid
+    accusamus quo quis, eum fuga, aut error tempora magni consequuntur
+    nam magnam vitae culpa aliquam aspernatur minus?
+</p>
+
+
+<?php include "footer.php"?>
