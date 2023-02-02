202fb062026923094d5c4ca653653939d1c80287
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-03 00:21:19 +0200

    added ex1.php

---
+++ Team13/ex1.php
@@ -0,0 +1,33 @@
+<?php
+$title = "Exercise : Hamk oy";
+include "header.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first page</title>
+</head>
+<body>
+    <h1>3.1 Write a simple PHP script to print your information (Name and your groupid). </h1>
+<?php
+//opening tag
+echo "Hosseinali Tahei Andani <br>";
+echo "BBCAP13<br>";
+?>
+<h1>3.2 Write PHP code to display the following message.</h1>
+<?php
+echo "Hello world! My name is \"David\""
+?>
+<h1>3.3 Current Date</h1>
+<?php
+echo Date("d.m.Y")
+?>
+<h1>3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document. </h1>
+<?php
+
+?>
+</body>
+</html>
+<?php include "footer.php";?>
