923281c0e3e126c6b8fee4e66ab0055f47563a6f
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-02 23:16:49 +0200

    added breakfast, lunch and dinner.php

---
+++ Team13/dinner.php
@@ -0,0 +1,25 @@
+<!--first row-->
+<div class="row">
+            <div class="row1">
+                <div class="col align-self-start"><img class="logo" src="images/logo.png" alt="logo"></div>
+                <div class="col align-self-center">
+                    <h2>Dinner Menu</h2>
+                </div>
+                <div class="col align-self-center"><a href="index.html"><img class="home" src="images/home (2).png" alt="home button"></a></div>
+            </div>
+ <!--second row-->
+            <div class="row2">
+                <div class="col align-self-center">
+                  <img class="left" src="images/ghaza-irani.jpg" alt="left image"></div>
+                <div class="col align-self-center">
+                    <div class="matn">Gheimeh Stew ( A mix of split peas, French fries and meat ) with Rice<div class="price">$12 </div></div>
+                    <div class="matn1"> Sabzi Polo with Fish<div class="price1"> $8.91</div></div>
+                    <div class="matn2"> Ghormeh Sabzi Stew( A mix of kidney beans, small pieces of meat and a herb) with Rice<div class="price2">$7 </div></div>
+                    <div class="matn3">Chicken Tah-chin( A mix of chopped chicken and rice)<div class="price3">$10 </div></div>
+                    <div class="matn4">Broad Bean Cooked Rice with Meat<div class="price4">$8.99 </div></div>
+                    <div class="matn5">Roasted Chicken with Cooked Rice<div class="price5">$9.95 </div></div>
+                    <div class="matn6">Bakhtiari Kebab with Cooked Rice<div class="price6">$10.95 </div></div>
+                </div>
+                <div class="col align-self-center"><img class="right" src="images/fesenjoon.jpg" alt="right image"></div>
+            </div>
+          </div>
