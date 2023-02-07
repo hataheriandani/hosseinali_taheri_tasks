cc8baefd400c662b55bf665dffe2e18125a66bb9
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-07 09:14:15 +0200

    create exercise 3

---
+++ layout/excercise3/variable.php
@@ -0,0 +1,9 @@
+<?php 
+include "header.php" ?>
+<h3>In-class Task Variable & Operators 07.02.2023 (variable.php)
+</h3>
+<h2>
+Create a simple html form to get Firstname and Lastname from the
+ user and use echo echo statement to print using <h3> 
+    tag: Hello …., You are welcome to my site.
+</h2>
