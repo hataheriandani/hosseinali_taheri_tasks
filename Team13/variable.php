d529ae0ec8c9d939d07bd35f2bbff2cfdc432123
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-07 10:31:31 +0200

    used a bootstrap example

--- Team13/variable.php
+++ Team13/variable.php
@@ -7,11 +7,18 @@ Create a simple html form to get Firstname and Lastname from the
     tag: Hello …., You are welcome to my site.
 </h2>
 <form action="action.php" method="post">
-    First Name: <input type="text" Name="fname"><br>
-    Last Name: <input type="text" Name="lname"><br>
+<div class="row">
+
+    <div class="col">
+     <input type="text" Name="fname" required placeholder="First Name" class="form-control">
+    </div>
+    <div class="col">
+     <input type="text" Name="lname" required placeholder="Last Name" class="form-control"></div>
+    </div>
     Birst Date: <input type="date" Name="date"><br>
     Select fv color: <input type="color" Name="color"><br>
     <input type="submit" value="submit"><br>
+    
 </form>
 
 <?php include "footer.php"?>
