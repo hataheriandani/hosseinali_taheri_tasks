33032c8b8fd7759387d16e0298068af35587cbc9
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-07 10:02:15 +0200

    complete the first question

--- layout/excercise3/action.php
+++ layout/excercise3/action.php
@@ -1,6 +1,13 @@
-<?php  include "header.php" ?>
+<?php include "header.php";
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$dob = $_POST['date'];
+$color = $_POST['color'];
 
 
-
+echo "<h3> Your name is $fname $lname and your date of birth is $dob
+and your favorite color $color </h3>";
+?>
 
 <?php include "footer.php"?>
--- layout/excercise3/variable.php
+++ layout/excercise3/variable.php
@@ -6,9 +6,9 @@ Create a simple html form to get Firstname and Lastname from the
  user and use echo echo statement to print using <h3> 
     tag: Hello …., You are welcome to my site.
 </h2>
-<form action="store.php" method="post">
+<form action="action.php" method="post">
     First Name: <input type="text" Name="fname"><br>
-    Last Name: <input type="text" Name="fname"><br>
+    Last Name: <input type="text" Name="lname"><br>
     Birst Date: <input type="date" Name="date"><br>
     Select fv color: <input type="color" Name="color"><br>
     <input type="submit" value="submit"><br>
