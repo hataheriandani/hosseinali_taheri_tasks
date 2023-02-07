1a458178f95411c66aef685c3660353d70ec7db1
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-07 09:41:51 +0200

    create form and action.php

--- layout/excercise3/variable.php
+++ layout/excercise3/variable.php
@@ -1,5 +1,4 @@
-<?php 
-include "header.php" ?>
+<?php include "header.php" ?>
 <h3>In-class Task Variable & Operators 07.02.2023 (variable.php)
 </h3>
 <h2>
@@ -7,3 +6,12 @@ Create a simple html form to get Firstname and Lastname from the
  user and use echo echo statement to print using <h3> 
     tag: Hello …., You are welcome to my site.
 </h2>
+<form action="store.php" method="post">
+    First Name: <input type="text" Name="fname"><br>
+    Last Name: <input type="text" Name="fname"><br>
+    Birst Date: <input type="date" Name="date"><br>
+    Select fv color: <input type="color" Name="color"><br>
+    <input type="submit" value="submit"><br>
+</form>
+
+<?php include "footer.php"?>
