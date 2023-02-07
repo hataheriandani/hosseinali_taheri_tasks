8b682843c5bbac43b09d49803929f45ee2cbf1bc
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-07 10:50:44 +0200

    do the third question

--- Team13/variable.php
+++ Team13/variable.php
@@ -20,5 +20,39 @@ Create a simple html form to get Firstname and Lastname from the
     <input type="submit" value="submit"><br>
     
 </form>
+<h2>
+Prepare a simple html table and apply bootstrap style to the table.
+</h2>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Birth</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Hosseinali</td>
+      <td>Taheri</td>
+      <td>1988</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Afsani</td>
+      <td>Emadi</td>
+      <td>1990</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Zahra</td>
+      <td>Mojiri</td>
+      <td>1970</td>
+    </tr>
+  </tbody>
+</table>
+
 
 <?php include "footer.php"?>
