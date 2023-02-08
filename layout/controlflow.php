aeb73a4f2b6428d0a111278a1647c83cb30d6e97
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-08 14:19:08 +0200

    Third question was solved with "If & elseif "

--- layout/controlflow.php
+++ layout/controlflow.php
@@ -1,11 +1,11 @@
 <?php include "header.php" ?>
 <h2>In-class Task Control flow and loops 08.02.2023 (controlflow.php)</h2>
-<h3>
+<h4>
 1- Write a script that gets the current month and prints one of the following responses,
  depending on whether it's August or not:
 It's August, so it's still holiday.
 Not August, this is Month-name so I don't have any holidays:
-</h3>
+</h4>
 <?php
 $main_month = date("F");
 
@@ -16,7 +16,7 @@ if ($main_month == "August") {
 }
 ?>
 <h4>
-Assign colour red to a variable name $color and check to print one the following responses (if else statement)
+2- Assign colour red to a variable name $color and check to print one the following responses (if else statement)
 The color is red. 
 The color is not red.
 </h4>
@@ -30,13 +30,29 @@ if ($color_bace == "red") {
 }
 ?>
 <h4>
-Write a program to grade students based on their total score for a subject. Use variable to hold the total score. The grading scheme is: 
+3- Write a program to grade students based on their total score for a subject. Use variable to hold the total score. The grading scheme is: 
 Excellent: >80; 
 Great > 70 & less than 80;
 Good > 60 & less than 70; 
 Pass > 50 & less than 60;
 Fail < 50 
 </h4>
+<?php
+$total_score = 75;
+
+if ($total_score > 80) {
+    echo "Excellent";
+} elseif ($total_score > 70 && $total_score < 80) {
+    echo "Great";
+} elseif ($total_score > 60 && $total_score < 70) {
+    echo "Good";
+} elseif ($total_score > 50 && $total_score < 60) {
+    echo "Pass";
+} else {
+    echo "Fail";
+}
+?>
+
 
 
 <?php include "footer.php"?>
