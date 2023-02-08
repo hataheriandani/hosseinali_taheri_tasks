5c6a91f7afe903ebca2d37b04877a339dfd5a6e0
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-08 13:43:10 +0200

    controlflow.php was created.

---
+++ layout/excercise3/controlflow.php
@@ -0,0 +1,11 @@
+<?php include "header.php" ?>
+<h3>In-class Task Control flow and loops 08.02.2023 (controlflow.php)</h3>
+<h2>
+Write a script that gets the current month and prints one of the following responses,
+ depending on whether it's August or not:
+It's August, so it's still holiday.
+Not August, this is Month-name so I don't have any holidays
+</h2>
+
+
+<?php include "footer.php"?>
