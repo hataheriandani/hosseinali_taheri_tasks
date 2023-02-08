6b2a37bec98f195449b4008a62a9b39cf74d7017
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-08 14:12:10 +0200

    Exercise 4 was created and first question solved.

--- layout/controlflow.php
+++ layout/controlflow.php
@@ -1,11 +1,22 @@
 <?php include "header.php" ?>
-<h3>In-class Task Control flow and loops 08.02.2023 (controlflow.php)</h3>
-<h2>
-Write a script that gets the current month and prints one of the following responses,
+<h2>In-class Task Control flow and loops 08.02.2023 (controlflow.php)</h2>
+<h3>
+1- Write a script that gets the current month and prints one of the following responses,
  depending on whether it's August or not:
 It's August, so it's still holiday.
-Not August, this is Month-name so I don't have any holidays
-</h2>
+Not August, this is Month-name so I don't have any holidays:
+</h3>
+<?php
+$main_month = date("F");
+
+if ($main_month == "August") {
+    echo "It's August, so it's still holiday.";
+} else {
+    echo "Not August, this is $main_month so I don't have any holidays.";
+}
+?>
+
+
 
 
 <?php include "footer.php"?>
