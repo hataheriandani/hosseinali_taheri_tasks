4cc176855f37092b4d3234bbe88899837d1c75ae
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-09 19:53:21 +0200

    Question 6 is edited.

--- layout/array.php
+++ layout/array.php
@@ -111,22 +111,45 @@ Recorded temperatures : 78, 60, 62, 68, 71, 68, 73,
  74, 62, 62, 65, 64, 68, 73, 75, 79, 73
 </h4>
 <?php
-$temperatures = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76,
-73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
-
-$sum = array_sum($temperatures);
-$count = count($temperatures);
-$average = $sum / $count;
-
-sort($temperatures);
-
-$l = array_slice($temperatures, 0, 5);
-
-$h = array_slice($temperatures, -5);
-
-echo "Average temperature: " . $average . "<br>";
-echo "Five lowest temperatures: " . implode(", ", $l) . "<br>";
-echo "Five highest temperatures: " . implode(", ", $h);
+echo "<hr><h2> Calculation average temperature: </h2>";
+$month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+// what is explode and what does the below code do? : 
+ /*The 'explode' function in PHP is a built-in function that is used to split a string into an array.
+   It takes two parameters: the string to be split and the delimiter (the character or string used to separate the string into pieces).*/
+$temp_array = explode(',', $month_temp);
+$tot_temp = 0;
+// What is count?
+ /*'count' is a built-in function in PHP that returns the number of elements in an array, or the number of characters in a string.
+    It can be used to determine the size of an array or string, which can be useful for looping through the elements or characters.*/
+$temp_array_length = count($temp_array);
+foreach($temp_array as $temp)
+{
+ $tot_temp += $temp;
+}
+ $avg_high_temp = $tot_temp/$temp_array_length;
+ echo "Average Temperature is : ".$avg_high_temp."
+"; 
+// what does sort do?
+/*The 'sort' function in PHP is a built-in function that sorts an array in ascending order. 
+  It rearranges the elements of the array so that they are listed in alphabetical or numerical order.
+  By default, 'sort' sorts arrays in ascending order based on the values of the elements.*/
+sort($temp_array);
+echo "<br> List of five lowest temperatures :";
+for ($i=0; $i< 5; $i++)
+{ 
+echo $temp_array[$i].", ";
+}
+echo "<br>List of five highest temperatures :";
+// explain the following loop
+/*The for loop in the given code is used to display the last five elements of the $temp_array array.
+  The loop starts with $i equal to ($temp_array_length-5) and continues until $i is less than ($temp_array_length).
+  In each iteration of the loop, $temp_array[$i] is printed, followed by a comma and a space.
+  The loop continues to run until $i is no longer less than ($temp_array_length).*/
+for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
+{
+echo $temp_array[$i].", ";
+}
 ?>
 
 <?php include "footer.php"?>
