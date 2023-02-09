88942544a607231b350ae5f0f71f613a86ae356b
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-09 11:21:35 +0200

    question 3 was done.

--- layout/array.php
+++ layout/array.php
@@ -9,11 +9,80 @@ include "header.php" ?>
 $courses = array("PHP", "HTML", "JavaScript", "CMS", "Project");
 
 echo "<ul>";
-foreach ($courses as $course) {
-  echo "<li>" . $course . "</li>";
-}
+for ($i = 0; $i < count($courses); $i++) 
+        {
+            echo "<li> $courses[$i] . <br>";
+        }
 echo "</ul>";
 ?>
+<h4>
+The unset() function is used to remove element from the array. The var_dump() function is used to dump information about a variable.  array_values() is an inbuilt function that returns all the values of an array and not the keys.
+Delete an element from the array below:
+$courses1=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+</h4>
+<?php
+$courses1 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+
+unset($courses1[4]);
+echo "<ul>";
+for ($i = 0; $i < count($courses1); $i++) 
+        {
+            echo "<li> $courses1[$i] . <br>";
+        }
+echo "</ul>";
+?>
+<h4>
+Sort the following array 
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+a) ascending order sort by value
+b) ascending order sort by Key
+c) descending order sort by Value
+d) descending order sort by Key
+</h4>
+<?php
+$courses3 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+
+asort($courses3);
+
+echo "Ascending order sort by value:<br>";
+for ($i = 0; $i < count($courses3); $i++) {
+    echo "<li>$courses3[$i] . <br>";
+}
+?>
+<?php
+$courses3 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+
+ksort($courses3);
+
+echo "Ascending order sort by key:<br>";
+for ($i = 0; $i < count($courses3); $i++) {
+    echo $courses3[$i] . "<br>";
+}
+?>
+<?php
+$courses3 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+
+arsort($courses3);
+
+echo "Descending order sort by value:<br>";
+for ($i = 0; $i < count($courses3); $i++) {
+    echo "<li>$courses3[$i] . <br>";
+}
+?>
+<?php
+$courses3 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+
+krsort($courses3);
+
+echo "Descending order sort by key:<br>";
+for ($i = 0; $i < count($courses3); $i++) {
+    echo $courses3[$i] . "<br>";
+}
+?>
+
+
+
+
 
 
 <?php include "footer.php"?>
