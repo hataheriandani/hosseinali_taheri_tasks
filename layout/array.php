5258d60f67c7712c9ed95f5d986c4da7364e65da
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-09 11:35:50 +0200

    Question4 was done.

--- layout/array.php
+++ layout/array.php
@@ -3,7 +3,7 @@ $title = "Contact Us : Hamk oy";
 include "header.php" ?>
 <h2> 5 In-class Task Arrays  09.02.2023 (array.php) </h2>
 <h4>
-1.  Write a php script to display courses as list. Use <li>
+1- Write a php script to display courses as list. Use <li>
 </h4>
 <?php
 $courses = array("PHP", "HTML", "JavaScript", "CMS", "Project");
@@ -16,7 +16,7 @@ for ($i = 0; $i < count($courses); $i++)
 echo "</ul>";
 ?>
 <h4>
-The unset() function is used to remove element from the array. The var_dump() function is used to dump information about a variable.  array_values() is an inbuilt function that returns all the values of an array and not the keys.
+2- The unset() function is used to remove element from the array. The var_dump() function is used to dump information about a variable.  array_values() is an inbuilt function that returns all the values of an array and not the keys.
 Delete an element from the array below:
 $courses1=array("PHP", "HTML", "JavaScript", "CMS", "Project");
 </h4>
@@ -32,7 +32,7 @@ for ($i = 0; $i < count($courses1); $i++)
 echo "</ul>";
 ?>
 <h4>
-Sort the following array 
+3- Sort the following array 
 $courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
 a) ascending order sort by value
 b) ascending order sort by Key
@@ -77,6 +77,19 @@ krsort($courses3);
 echo "Descending order sort by key:<br>";
 for ($i = 0; $i < count($courses3); $i++) {
     echo $courses3[$i] . "<br>";
+}
+?>
+<h4>
+4- Change the following array's all values to upper case.
+ $courses4=array("php", "html", "javascript", "cms", "project");
+</h4>
+<?php
+$courses4 = array("php", "html", "javascript", "cms", "project");
+
+$courses4 = array_map("strtoupper", $courses4);
+
+for ($i = 0; $i < count($courses4); $i++) {
+    echo $courses4[$i] . "<br>";
 }
 ?>
 
