0f468909a5ee586651985729f4a93b2b90e59931
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-22 13:11:38 +0200

    jsinclass.php is created

---
+++ layout/jsinclass.php
@@ -0,0 +1,11 @@
+<?php
+$tirle = "JavaScript Events and validation";
+include "header.php"
+?>
+<h2>A simple button to display date</h2>
+<button onclick="this.innerHTML = date()">Click to know date </button>
+
+
+<?php
+include "footer.php"
+?>
