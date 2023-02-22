10ea8fbf284d20e7dceaaff61f32149cccf09836
hataheriandani <hosseinali22000@student.hamk.fi>
2023-02-22 13:20:15 +0200

    Make a button and a place holder as paragraph for the date

--- layout/jsinclass.php
+++ layout/jsinclass.php
@@ -3,7 +3,11 @@ $tirle = "JavaScript Events and validation";
 include "header.php"
 ?>
 <h2>A simple button to display date</h2>
-<button onclick="this.innerHTML = date()">Click to know date </button>
+<button onclick="this.innerHTML = Date()">Click to know date </button>
+<h2>Make a button and a place holder as paragraph for the date </h2>
+<button onclick="getElementById('p1').innerHTML = Date()">Click to know date </button>
+<p id="p1"></p>
+
 
 
 <?php
